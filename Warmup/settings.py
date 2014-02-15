5221eb6118440e1e2b884b6608352543ef53ac81
raidersss <oaklandsportsrule@gmail.com>
2014-02-14 16:07:17 -0800

    added test server to settings

--- Warmup/settings.py
+++ Warmup/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -63,6 +64,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
     }
 }
+
+if 'test' in sys.argv:
+    DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
--- testAdditional.py
+++ testAdditional.py
@@ -10,25 +10,25 @@ class TestAddUser(testLib.RestTestCase):
     ERR_BAD_USERNAME    = -3     #: (for add, or login) invalid user name (only empty string is invalid for now)
     ERR_BAD_PASSWORD    = -4
     """Testing adding users"""
-    def correctAdd(self):
+    def testcorrectAdd(self):
         """Adds a new user."""
         response = self.makeRequest("/user/add",method="POST", data = { 'user':'test1','password':'1234'})
         self.assertDictEqual(response,{'errCode':SUCCESS,'count':1})
 
-    def repeatAdd(self):
+    def testrepeatAdd(self):
         """adds the same user"""
         init = self.makeRequest("/user/add",method="POST", data = { 'user':'test2','password':'1234'})
         response = self.makeRequest("/user/add",method="POST", data = { 'user':'test2','password':'1234'})
         self.assertDictEqual(response,{'errCode':ERR_USER_EXISTS})
 
-    def lengthCheckUser(self):
+    def testlengthCheckUser(self):
         response = self.makeRequest("/user/add",method="POST", data = { 'user':'testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest','password':'1234'})
         self.assertDictEqual(response,{'errCode':ERR_BAD_USERNAME})
         response = self.makeRequest("/user/add",method="POST", data = { 'user':'','password':''})
         self.assertDictEqual(response,{'errCode':ERR_BAD_USERNAME})
 
 
-    def lengthCheckPass(self):
+    def testlengthCheckPass(self):
         response = self.makeRequest("/user/add",method="POST", data = { 'user':'longpass','password':'testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest'})
         self.assertDictEqual(response,{'errCode':ERR_BAD_PASSWORD})
 
@@ -39,23 +39,23 @@ class TestLogin(testLib.RestTestCase):
     ERR_BAD_USERNAME    = -3     #: (for add, or login) invalid user name (only empty string is invalid for now)
     ERR_BAD_PASSWORD    = -4
 
-    def loginSuccess(self):
+    def testloginSuccess(self):
         init = self.makeRequest("/user/add",method="POST", data = { 'user':'test3','password':'1234'})
         response = self.makeRequest("/user/login",method="POST", data = { 'user':'test3','password':'1234'})
         self.assertDictEqual(response,{'errCode':SUCCESS,'count':1})
 
-    def counter(self):
+    def testcounter(self):
         init = self.makeRequest("/user/add",method="POST", data = { 'user':'test4','password':'1234'})
         first = self.makeRequest("/user/login",method="POST", data = { 'user':'test4','password':'1234'})
         second = self.makeRequest("/user/login",method="POST", data = { 'user':'test4','password':'1234'})
         third = self.makeRequest("/user/login",method="POST", data = { 'user':'test4','password':'1234'})
         self.assertDictEqual(third,{'errCode':SUCCESS,'count':3})
 
-    def loginFailUser(self):
+    def testloginFailUser(self):
         response = self.makeRequest("/user/login",method="POST", data = { 'user':'test5','password':'1234'})
         self.assertDictEqual(response, {'errCode':ERR_BAD_CREDENTIALS})
 
-    def loginFailPass(self):
+    def testloginFailPass(self):
         init = self.makeRequest("/user/add",method="POST", data = { 'user':'test6','password':'1234'})
         response = self.makeRequest("/user/login",method="POST", data = { 'user':'test6','password':'abcd'})
         self.assertDictEqual(response, {'errCode':ERR_BAD_CREDENTIALS})
